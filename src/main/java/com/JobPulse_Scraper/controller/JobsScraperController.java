7f8b843205228cd46abbcd543b32eca5cdb9b2da
udaykkarnam <udaykumark206@gmail.com>
2026-05-30 13:53:49 +0530

    added log info in restcontroller class

--- src/main/java/com/JobPulse_Scraper/controller/JobsScraperController.java
+++ src/main/java/com/JobPulse_Scraper/controller/JobsScraperController.java
@@ -3,11 +3,13 @@ package com.JobPulse_Scraper.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class JobsScraperController {
 
 
@@ -15,6 +17,7 @@ public class JobsScraperController {
     @GetMapping("/welcome")
     public String JobsScraperDoneWelcomePage(){
 
+        log.info("Welcome to JobsScraperController");
         return "Welcome to Jobs Scraper !";
     }
 

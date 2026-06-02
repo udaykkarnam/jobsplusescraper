784b2ebd7b1e2ff2c72da96f2c89777c91682e00
udaykkarnam <udaykumark206@gmail.com>
2026-06-02 07:54:56 +0530

    removed printing duplicates urls on screen

--- src/main/java/com/JobPulse_Scraper/controller/JobsScraperController.java
+++ src/main/java/com/JobPulse_Scraper/controller/JobsScraperController.java
@@ -4,6 +4,7 @@ package com.JobPulse_Scraper.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
